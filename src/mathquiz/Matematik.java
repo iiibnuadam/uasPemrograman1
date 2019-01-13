fb166bcc9acf1f62731ec334e70093b2c6206ae3
iiibnuadam <iiibnuadam@gmail.com>
2019-01-14 00:39:14 +0700

    fix

---
+++ src/mathquiz/Matematik.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathquiz;
+
+public class Matematik {
+    int temp;
+	public int tambah(int a, int b) {
+		return a + b;
+	}
+	public int kurang(int a, int b) {
+		return a - b;
+	}
+	public int kali(int a, int b) {
+		return a * b;
+	}
+	public int kelilingPersegPanjang(int a, int b) {
+		return 2 * (a + b);
+	}
+	public int luasSegitiga(int a, int b) {
+		return a * b / 2;
+	}
+}
